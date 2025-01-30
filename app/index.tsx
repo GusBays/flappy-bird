16b0d71bd74f5a59a00914230734cd45935b5eda
Gustavo Bays <gustavo_boliv@hotmail.com>
2025-01-30 18:10:10 -0300

    feat: adicionado load da font com expo-fonrt

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
 import { BACKGROUND_COLOR } from '@/src/@constants/background-color'
 import Home from '@/src/views/Home'
+import { useFonts } from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
 import { useCallback, useEffect } from 'react'
 import { ImageBackground } from 'react-native'
-import BACKGROUND from '../src/assets/background.png'
+import PRESS_START_2P from '../assets/fonts/PressStart2P-Regular.ttf'
+import BACKGROUND from '../src/assets/images/background.png'
 
 export default function Index() {
+    useFonts({ PressStart2P: PRESS_START_2P })
+
     const hideSplashScreen = useCallback(async () => await SplashScreen.hideAsync(), [])
     const hideSplashScreenWithDelay = useCallback(() => {
         const timeInMilliseconds = 1500
