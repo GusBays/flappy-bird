9c2f10daf1e4aab8f0e511e11b39ec58338ed786
Gustavo Bays <gustavo_boliv@hotmail.com>
2025-01-30 00:37:57 -0300

    feat: adicionada imagem de background na entrada do router

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,24 @@
+import { BACKGROUND_COLOR } from '@/src/@constants/background-color'
 import Home from '@/src/views/Home'
 import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
 import { useCallback, useEffect } from 'react'
+import { ImageBackground } from 'react-native'
+import BACKGROUND from '../src/assets/background.png'
 
 export default function Index() {
     const hideSplashScreen = useCallback(async () => await SplashScreen.hideAsync(), [])
     const hideSplashScreenWithDelay = useCallback(() => {
-        const timeInMilliseconds = 3000
+        const timeInMilliseconds = 1500
         setTimeout(hideSplashScreen, timeInMilliseconds)
     }, [])
 
     useEffect(hideSplashScreenWithDelay)
 
     return (
-        /** @ts-ignore */
-        <>
+        <ImageBackground source={BACKGROUND} resizeMode="cover" style={{ flex: 1, backgroundColor: BACKGROUND_COLOR }}>
             <StatusBar style="auto" hidden />
             <Home />
-        </>
+        </ImageBackground>
     )
 }
