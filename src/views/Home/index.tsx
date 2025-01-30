68fe8e08015b99b5dd08e0825deb963a856e0ad1
Gustavo Bays <gustavo_boliv@hotmail.com>
2025-01-30 00:42:38 -0300

    feat: inclusao das telas baseado no estado do jogo

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,26 +1,49 @@
-import entities from '@/src/entities'
+import { Entities, Physics } from '@/src/entities'
 import { useRef, useState } from 'react'
 import { GameEngine } from 'react-native-game-engine'
+import GameOver from '../GameOver'
+import Start from '../Start'
 import { styles } from './styles'
 
 const Home = () => {
     const [running, setIsRunning] = useState(false)
+    const [gameOver, setIsGameOver] = useState(false)
+
     const engineRef = useRef()
 
-    const onPressStart = () => {}
+    const onStart = () => {
+        setIsRunning(true)
+        setIsGameOver(false)
+    }
 
-    // return (
-    //     <ImageBackground source={BACKGROUND} resizeMode="cover" style={styles.container}>
-    //         <Start onPress={onPressStart} />
-    //     </ImageBackground>
-    // )
+    const onGameOver = () => {
+        setIsRunning(false)
+        setIsGameOver(true)
+    }
+
+    const onRestart = () => {
+        setIsGameOver(false)
+    }
+
+    const onEvent = (e: Event) => {
+        switch (e.type) {
+            case 'game_over':
+                onGameOver()
+                break
+        }
+    }
+
+    if (false === running && false === gameOver) return <Start onStart={onStart} />
+    if (false === running && gameOver) return <GameOver onRestart={onRestart} />
 
     return (
         <GameEngine
             /** @ts-ignore */
             ref={engineRef}
             running={running}
-            entities={entities()}
+            entities={Entities()}
+            systems={[Physics]}
+            onEvent={onEvent}
             style={styles.engine}
         />
     )
