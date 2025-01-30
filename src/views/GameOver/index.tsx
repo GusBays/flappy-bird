45ffaf7011e940fd07bdab06ab0c116810881dd7
Gustavo Bays <gustavo_boliv@hotmail.com>
2025-01-30 00:41:22 -0300

    feat: criada tela, tipagens e estilos para GameOver

---
+++ src/views/GameOver/index.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import { Image, View } from 'react-native'
+import GAME_OVER from '../../assets/game-over.png'
+import { styles } from './styles'
+import { GameOverProps } from './types'
+
+const GameOver: React.FC<GameOverProps> = ({ onRestart }) => {
+    useEffect(() => {
+        const timeInMilliSeconds = 2500
+        setTimeout(onRestart, timeInMilliSeconds)
+    })
+
+    return (
+        <View style={styles.container}>
+            <Image source={GAME_OVER} style={styles.image} />
+        </View>
+    )
+}
+
+export default GameOver
